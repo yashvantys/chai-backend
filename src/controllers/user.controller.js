13e036567cb7c5e733736df244eda050fcbbd54b
Yashvant Yadav <yashvanty@gmail.com>
2023-12-24 08:45:56 +0530

    get channel user API added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,6 +205,67 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     }, { new: true }).select("-password");
     return res.status(200).json(new ApiResponse(200, user, "Cover Image updated successfully"))
 })
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+    const channel = await User.aggregate([
+        {
+            $match: { username: username?.toLowerCase() }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribeToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribeToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ]);
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel does not exists!")
+    }
+    return res.status(200).json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
+})
 export {
     registerUser,
     login,
@@ -214,5 +275,6 @@ export {
     getCurrentUser,
     updateUser,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
