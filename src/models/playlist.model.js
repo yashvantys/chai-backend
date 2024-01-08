12cf4d79f5d9b4c68cbc0e9107c401c17d2eb20b
Yashvant Yadav <yashvanty@gmail.com>
2024-01-08 13:05:24 +0530

    Models added

---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            type: Schema.Types.owner,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: Schema.Types.owner,
+        ref: "User"
+    }
+},
+    { timestamps: true }
+)
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
