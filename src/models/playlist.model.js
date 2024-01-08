6cc184c2f4f846978d646e72eaa27df6c163e99d
Yashvant Yadav <yashvanty@gmail.com>
2024-01-08 13:07:31 +0530

    model formats

--- src/models/playlist.model.js
+++ src/models/playlist.model.js
@@ -23,4 +23,4 @@ const playlistSchema = new Schema({
     { timestamps: true }
 )
 
-export const Playlist = mongoose.model("Playlist", playlistSchema)
+export const Playlist = mongoose.model("Playlist", playlistSchema);
--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -11,4 +11,4 @@ const tweetSchema = new Schema({
     }
 }, { timestamps: true })
 
-export const Tweet = mongoose.model("Tweet", tweetSchema)
+export const Tweet = mongoose.model("Tweet", tweetSchema);
